f6ff33ad9ab29c72ab9663069edb8a84de0b9cb2
Om Prakash Behera <84368369+coderom2009@users.noreply.github.com>
2023-03-29 11:43:18 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDr4qkU9RaO-Y5rXIuXlTQnqt7dyD9ZzaY",
+  authDomain: "e-ride-833b2.firebaseapp.com",
+  projectId: "e-ride-833b2",
+  storageBucket: "e-ride-833b2.appspot.com",
+  messagingSenderId: "634314550244",
+  appId: "1:634314550244:web:a7f38f5ed747eb94faf5f1"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
